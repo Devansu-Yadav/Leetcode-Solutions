8b485d57f8cdd7eb4aa6a75b4e5af46f69bbb31d
Devansu <devansuyadav@gmail.com>
2022-01-24 11:58:46 +0530

    24-01-2022: Re-solved 1 LC Med Koko Eating Bananas

--- Searching Assignment/KokoEatingBananas.java
+++ Searching Assignment/KokoEatingBananas.java
@@ -1,13 +1,16 @@
 // Problem - https://leetcode.com/problems/koko-eating-bananas/
+
 import java.util.Arrays;
 import java.lang.Math;
 
+// T.C - O(Nlog(maxVal - 1))
 public class KokoEatingBananas {
     public int minEatingSpeed(int[] piles, int h) {
-        return minSpeed(piles, h);
+        return minSpeed1(piles, h);
+        // return minSpeed2(piles, h);
     }
     
-    public static int minSpeed(int[] piles, int h) {
+    public static int minSpeed1(int[] piles, int h) {
         int start = 1;
         int end = Arrays.stream(piles).max().getAsInt();
         
@@ -26,5 +29,43 @@ public class KokoEatingBananas {
             }
         }
         return start;
+    }
+
+    public static int minSpeed2(int[] piles, int h) {
+        int start = 1;
+        int end = Arrays.stream(piles).max().getAsInt();
+        int res = 0;
+        
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            
+            if(canEatBananas(piles, h, mid)) {
+                res = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return res;
+    }
+    
+    public static boolean canEatBananas(int[] piles, int h, int k) {
+        int hr = 0;
+        
+        for(int i = 0; i<piles.length; i++) {
+            if(piles[i] <= k) {
+                hr ++;
+            } else if(piles[i] % k == 0) {
+                hr += (piles[i] / k);
+            } 
+            else if(piles[i] % k > 0) {
+                hr += (piles[i] / k + 1);
+            }
+        }
+        
+        if(hr <= h) {
+            return true;
+        }
+        return false;
     }
 }
